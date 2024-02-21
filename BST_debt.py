47f4a2e9cedb4e87a30f699a89a6b6203d1eef07
eas613 <131161187+eas613@users.noreply.github.com>
2024-02-21 13:18:34 +0200

    Added add_customer function. Added add_customer function to add customer ordered by debt.

--- BST_debt.py
+++ BST_debt.py
@@ -1,7 +1,31 @@
-from customer import Customer
+from customer1 import Customer
 from typing import Optional
 
 class Customer_BST:
     def __init__(self) -> None:
-        self._root:Optional[Customer] = None
+        self._root = None
+    
+    def add_customer(self,customer:Customer):
+        if not self._root:
+            self._root = customer
+            return
+        
+        debt = customer.debt
+        temp = self._root
+        while True:
+            temp_debt = temp.debt
+            if debt <= temp_debt:
+                if not temp._left_debt:
+                    temp._left_debt = customer
+                    return
+                temp = temp._left_debt
+            else:
+                if not temp._right_debt:
+                    temp._right_debt = customer
+                    return
+                temp = temp._right_debt
+        
+  
+    
+        
     
